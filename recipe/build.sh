69c71fba2fa1c16f91d0a8c8772ccca66353ed6b
Chris Hogan <c096h800@gmail.com>
2017-12-14 16:00:41 -0600

    Add mpi

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,9 @@
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CFLAGS="${CFLAGS} -I${PREFIX}/include"
 
+export CC=${PREFIX}/bin/mpicc
+export CXX=${PREFIX}/bin/mpic++
+
 ./configure --prefix=$PREFIX --enable-shared --enable-mpi
 make
 make install
